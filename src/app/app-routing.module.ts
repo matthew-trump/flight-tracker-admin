a72e2efda513919a904b851726c03a4e72b920cc
matthew-trump <matthew.trump@gmail.com>
2019-05-03 12:41:23 -0700

    add basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { TestApiComponent } from './test-api/test-api.component';
+import { AdminComponent } from './admin/admin.component';
+import { AuthGuard } from './auth.guard';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'login', pathMatch: 'full', component: LoginComponent },
+  { path: 'test-api', pathMatch: 'full', component: TestApiComponent },
+  {
+    path: 'admin', canActivate: [AuthGuard],
+    children: [
+      { path: '', pathMatch: 'full', component: AdminComponent },
+    ]
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
