c41699a88f5d5c3af73753a060bc560049161160
matthew-trump <matthew.trump@gmail.com>
2019-05-03 12:41:54 -0700

    core logic for base component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { BackendService } from './backend.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'flight-tracker-admin';
+
+  loggedInRoutes: any[] = [
+    { route: ['admin'], label: 'Admin' }
+  ]
+
+  constructor(
+    public authService: AuthService,
+    public backendervice: BackendService,
+    private router: Router) {
+  }
+
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
+
 }
